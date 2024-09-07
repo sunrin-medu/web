9724f6cf8123a381776d503691ac32de990d571c
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-09-07 22:26:49 +0900

    fix: https 오류

--- public/unity/unity.framework.js
+++ public/unity/unity.framework.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2cff68aed31193166dd43fd32d4ef0a80aa4b9c01f5466995c5ba6826956d3e
-size 411027
+oid sha256:e9085c5646ede47751b562e60fbb51e298ffde99edbdf6ae5a6704aa2365fabb
+size 405975
