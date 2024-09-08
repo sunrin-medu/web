b5f9c863344ccfb1760717b24c5c5a2493ab14c9
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-09-09 08:50:23 +0900

    fix: 무한 로그인

--- src/app/login/[provider]/page.tsx
+++ src/app/login/[provider]/page.tsx
@@ -23,7 +23,9 @@ export default function Page({ params }: { params: { provider: string } }) {
 	};
 
 	useEffect(() => {
-		handleSignIn();
+		if (status === 'unauthenticated') {
+			handleSignIn();
+		}
 	}, [ status ]);
 
 	return (
