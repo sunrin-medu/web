eb385485e7037e1935cd1f4628fdca200db16e8c
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-09-09 01:02:45 +0900

    fix: apply error

--- public/unity/unity.framework.js
+++ public/unity/unity.framework.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54e93cda74901dfbc592f8beaac02f8525b4eccd94ed4ac96e67365dbccd8030
-size 405976
+oid sha256:8d99c083063dca3bb5f366f541ac0bc2fcf012cad45cdd29b778ef23a4f63983
+size 747857
