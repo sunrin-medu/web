281eb3d675bd82947382011d44f87a50cc49b2de
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-09-10 01:06:12 +0900

    add: rotation 서버

--- public/unity/unity.framework.js
+++ public/unity/unity.framework.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be0052cc623154e5b97b4da9e392cbee79283d139dc52ea1b8afa3e62fb1d35f
+oid sha256:6cf452f9108dc155b881a488b25de954d953171c6c8274c2b692969fd1139b67
 size 416347
