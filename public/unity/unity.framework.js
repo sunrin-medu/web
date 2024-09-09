3a9d07a1dbb112675b9d621b03f1cc7cddbf341c
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-09-09 23:57:04 +0900

    fix: 플레이어 컨트롤

--- public/unity/unity.framework.js
+++ public/unity/unity.framework.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cded5b91072898af58bbc96b6ed19e35a7f96cce31ca711a03ea9cf3b989874c
+oid sha256:be0052cc623154e5b97b4da9e392cbee79283d139dc52ea1b8afa3e62fb1d35f
 size 416347
